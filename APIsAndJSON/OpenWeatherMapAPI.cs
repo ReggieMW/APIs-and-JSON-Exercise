618ea14801c642c5a4f370afce79b56772b3a4ad
ReggieMW <reggiewoods@msn.com>
2025-06-15 13:49:40 -0500

    cannot get a valid url to call from

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    internal static class OpenWeatherMapAPI
     {
+        public static void GetUserTemp()
+        {
+            var client = new HttpClient();
+            var appsettingsText = File.ReadAllText("appsettings.json");
+
+            var apiKey = "d230c0619c81262007a55421f21d88"; 
+            
+            Console.WriteLine("Enter Zip:");
+            var zip = Console.ReadLine();
+
+            var url = $"https://api.openweathermap.org/data/2.5/weather?zip={zip}&appid={apiKey}";
+            
+
+            var response = client.GetStringAsync(url).Result;
+            
+            var weatherObject = JObject.Parse(response);
+
+            Console.WriteLine(
+                $"Current Temp: {weatherObject["main"]["temp"]}\nFeels_Like: {weatherObject["main"]["feels_like"]}");
+        }
     }
 }
